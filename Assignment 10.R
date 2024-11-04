5671e4d35daf16a2455b5b7261970ac04adeb180
Gokul915 <kolligokulkrishna@gmail.com>
2024-11-03 23:16:12 -0500

    Add files via upload

---
+++ Assignment 10.R
@@ -0,0 +1,7 @@
+package: Gokul
+Title: Geovisuals
+Authors: Gokul krishna kolli gokulkrishnak@usf.edu
+Description: This package designed for spatial and temporal trend analysis of geospatial data. It enables researchers and data analysts to uncover patterns and trends over time within geographic datasets,making it easier to visualize and understand changes in version phenomena
+Version: 0.1.0
+Depends: R3.1.2
+License: MIT
